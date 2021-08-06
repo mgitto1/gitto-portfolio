308da95edfb9085ce684f6ae441e762d1950349b
mgitto1 <matthew.gitto1@gmail.com>
2021-08-06 17:43:07 -0400

    Intro section done, styling needed

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './intro.scss';
+import {init} from "ityped"
 
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  const textRef = useRef()
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      strings: ["Developer", "Designer", "Engineer"]
+    })
+  },[])
+  return (
+  <div className="intro" id="intro">
+    <div className = "left">
+      <div className= "imgContainer">
+        <img src="assets/matt.png" alt="Picture of me" />
+      </div>
+    </div>
+    <div className = "right">
+      <div className = "wrapper">
+        <h2>Hi There, I'm</h2>
+        <h1>Matt Gitto</h1>
+        <h3>Aspiring <span ref={textRef}></span></h3>
+      </div>
+      <a href="#portfolio">
+      <img src="assets/downarrow.png" alt="down arrow" />
+      </a>
+    </div>
+  </div>
+  )
 }
