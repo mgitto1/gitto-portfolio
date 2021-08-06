f16db11739abd5e51b424c83e5ba4f7a690b1e0a
mgitto1 <matthew.gitto1@gmail.com>
2021-08-06 18:22:12 -0400

    portfolio tile placeholder added

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -2,5 +2,22 @@ import React from 'react';
 import './portfolio.scss';
 
 export default function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+  <div className="portfolio" id="portfolio">
+    <h1>Portfolio</h1>
+    <ul>
+      <li className="active">Featured</li>
+      <li>Web App</li>
+      <li>Mobile App</li>
+      <li>Design</li>
+      <li>Branging</li>
+    </ul>
+    <div className="container">
+      <div className="item">
+        <img src="assets/logo192.png" alt="placeholder" />
+        <h3>App</h3>
+      </div>
+    </div>
+  </div>
+  )
 }
