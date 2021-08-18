8137ad1803305711a3c0b24e6bed21a9a3470b66
mgitto1 <matthew.gitto1@gmail.com>
2021-08-18 15:49:43 -0400

    changed route orders to fix email

--- server/index.js
+++ server/index.js
@@ -10,17 +10,17 @@ app.use(cookieParser());
 
 const { sendEmail } = require('./mail');
 
+app.post('/api/sendMail', (req, res) => {
+  console.log(req.body);
+  sendEmail('matthew.gitto1@gmail.com', req.body.email, req.body.message);
+});
+
 // static file-serving middleware
 app.use(express.static(path.join(__dirname, '..', 'react-ui/build')));
 
 // sends index.html
 app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'react-ui/public/index.html'));
-});
-
-app.post('/api/sendMail', (req, res) => {
-  console.log(req.body);
-  sendEmail('matthew.gitto1@gmail.com', req.body.email, req.body.message);
 });
 
 app.listen(process.env.PORT || 5000, () => {
